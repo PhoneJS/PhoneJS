0632b3cc5ee950a409778b00647518b2d27846ae
PhoneJS <contaphonejs@gmail.com>
2025-07-27 19:32:42 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -2,5 +2,3 @@
 javascript:(function(){var s=document.createElement('script');s.src='https://phonejs.github.io/PhoneJS/Layout.js';document.body.appendChild(s);})()
 
 javascript:(function(){var s=document.createElement('script');s.src='https://phonejs.github.io/PhoneJS/Phone.js';document.body.appendChild(s);})()
-
-javascript:(function(){var s=document.createElement('script');s.src='https://phonejs.github.io/PhoneJS/index.js';document.body.appendChild(s);})()
